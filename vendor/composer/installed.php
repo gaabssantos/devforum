ad4c189261c663429fa533f54616142e98dc07ce
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-10-13 12:53:39 -0300

    feat(Installed composer): :sparkles:

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/forum',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '940acf05fb10fb1b70126f6dd058533e69640c62',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/forum' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '940acf05fb10fb1b70126f6dd058533e69640c62',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
